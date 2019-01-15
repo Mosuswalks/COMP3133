e59774ef3750ce761df7d7df83361db527bfa073
Mohamed Abdi <mosuswalks@gmail.com>
2019-01-15 15:58:17 -0500

    Added Lab1

---
+++ COMP3133_Lab1.js
@@ -0,0 +1,38 @@
+
+//First Question
+function capitlizeFirstLetter(inputString){
+    return inputString.replace(/\w\S*/g, function(str){return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();})
+}
+
+
+
+// Second Question
+function largestInteger(num1,num2,num3){
+    return num1 > num2 && num1 > num3 ? num1
+            : num2 > num1 && num2 > num3 ? num2
+            : num3 > num1 && num3 > num2 ? num3
+            : "The numbers provided are all equal";
+}
+
+console.log(largestInteger(5,5,5))
+
+// Third Question
+function moveLast3Char(inputString){
+    return inputString.substr(-3, 3) + inputString.substr(0, inputString.length - 3);
+}
+
+// Fourth Question
+function getAngleType(angle){
+    return angle >= 0 && angle < 90 ? "Acute angle"
+         : angle == 90 ? "Right angle"
+         : angle > 90 && angle < 180 ? "Obtuse angle"
+         : angle == 180 ? "Straight"
+         : "Please enter a valid Degree input";
+}
+
+
+console.log(capitlizeFirstLetter('Testing'));
+console.log(largestInteger(5,5,5));
+console.log(moveLast3Char('Python'));
+console.log(getAngleType(180));
+
